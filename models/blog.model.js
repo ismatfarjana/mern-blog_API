47fe09215442985bde0fbf5e2251ada6e5be9e06
Syeda Ismat Farjana <diptyfmc17@gmail.com>
2020-10-07 22:37:33 +1100

    Code cleaning in blogs Schema

--- models/blog.model.js
+++ models/blog.model.js
@@ -1,11 +1,6 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const commentSchema = new Schema({
-  comment: { type: String, required: true },
-  name: { type: String, required: true }
-});
-
 const blogSchema = new Schema(
   {
     title: { type: String, required: true },
